34a9193d682eee20823a94985ea1942419b36293
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2021-09-20 22:57:48 +0900

    スクレイピング時の文字コードの取得

---
+++ chapter2/requests_header_encoding.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import sys
+import requests
+
+url = sys.argv[1]#第一引数からURLを取得する
+r = requests.get(url)#URLで指定されたwebページを取得する
+r.encoding = r.apparent_encoding #バイト列の特徴から推定したエンコーディングを使用する
+print(f'encoding: {r.encoding} ', file=sys.stderr)#エンコーディングを標準エラー出力に出力する
+print(r.text)#デコードしたレスポンスボディを標準出力に出力する
+
